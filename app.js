ae4dcc90e6f9f31b3f31fccb1ade48ffdaf5c5ca
unknown <japerez.ph@gmail.com>
2019-04-25 13:47:32 +0800

    callback chaining, object property shorthand and destructuring

--- app.js
+++ app.js
@@ -1,38 +1,28 @@
-const request = require('request')
-const chalk = require('chalk')
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const chalk = require('chalk')
+
+const address = process.argv[2]
+
+    if(!address) {
+        return console.log(chalk.red.inverse("Please provide address!"))
+    } 
+        geocode(address, (error, { latitude, longitude, location }) => { 
+
+            if (error) {
+                return console.log(error)
+            } 
+
+            forecast(latitude, longitude, (error, forecastData) => {
+                if (error) {
+                    return console.log(error)
+                }
+                
+                console.log(chalk.yellow.bold.underline(location))
+                console.log(chalk.yellow(forecastData))  
+            })   
+        })
 
 
-geocode('Philadelphia', (error, data) => { 
-    console.log('Error: ', error)
-    console.log('Data: ', data)
-})
 
-    // const url = "https://api.darksky.net/forecast/3a86fdc7e15ad649157a33abfee09437/37.8267,-122.4233?units=si&lang=de"
-
-//argument 1: options object outlines what we want to do
-//argument 2: function to run once the data is available
-// request({ url: url, json: true }, (error, response) => {
-//     //no need to parse because we set request package's json to true
-
-//     // console.log(error); //check for existence of error object
-
-//     if(error) {
-//         console.log(chalk.red.bold.inverse("Unable to connect to weather service!"))
-//     } else if (response.body.error) {
-//         //if there is a low level os error, print the following message
-//         console.log(chalk.blue.bold.inverse("Unable to find location"))
-
-//     } else {
-//         const temp = response.body.currently.temperature 
-//         const preciptProb = response.body.currently.precipProbability 
-//         console.log(
-//             response.body.daily.data[0].summary
-//         +    " It is currently " 
-//         +  temp 
-//         + " degrees Celsius out. There is a " 
-//         + preciptProb 
-//         + "% chance of rain.")
-//     }
-// })
 
--- utils/geocode.js
+++ utils/geocode.js
@@ -5,30 +5,28 @@ const geocode = (address, callback) => {
     /*pass address into a function called encodeURIComponent 
     to prevent the program from crashing when someone location 
     that contains special characters that means something in a url structure like ? */
-    const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/" 
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" 
     + encodeURIComponent(address) 
     + ".json?access_token=pk.eyJ1Ijoiam95Y2VwZXJleiIsImEi" 
     + "OiJjanV1eW14OXAwZWg4NDlueTZwcTZvOGduIn0.0YdkKZKKRKrcazOQlBWJvg&limit=1"
 
-    request({ url : geocodeURL, json: true}, (error, response) => {
+    request({ url, json: true}, (error, { body }) => {
 
             if(error){
-                callback(chalk.red.inverse("Unable to connect!", undefined))
-            } else if (response.body.features.length === 0) {
+                callback(chalk.red.inverse("Unable to connect!"), undefined)
+            } else if (body.features.length === 0) {
                 callback(chalk.blue.inverse("No matching result!"), undefined)
             } else {
                 // console.log(response.body.features.length) // 0
-                const long = response.body.features[0].center[0]
-                const lat = response.body.features[0].center[1]
-                const searchKey = response.body.query
-                const place = searchKey.join().replace(",", " ")
-                callback(undefined, "The longitude and latitude of " 
-                    + place 
-                    + " is " 
-                    + long 
-                    + " and " 
-                    + lat 
-                    + ", respectively.")
+                const longitude = body.features[0].center[0]
+                const latitude = body.features[0].center[1]
+                /*lat, long, and loc are inside an obj 
+                so they can be accessed later on*/
+                callback(undefined, {
+                    latitude, 
+                    longitude, 
+                    location: body.features[0].place_name
+                })
             }
         })
 }
